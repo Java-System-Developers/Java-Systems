e7d3ef3389d70110855e9b4bcb2c67e291a65eb6
LanseylowT <50694634+LanseylowT@users.noreply.github.com>
2023-01-14 16:44:56 +0800

    With UpdateData madafaka

---
+++ src/Database/Data/GetData.java
@@ -0,0 +1,16 @@
+package Database.Data;
+
+import java.util.List;
+
+public class GetData {
+    public List<List<String>> GetUserData() {
+
+
+        return null;
+    }
+
+    public List<List<String>> GetProductData() {
+
+        return null;
+    }
+}
