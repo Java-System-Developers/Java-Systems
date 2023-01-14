fe1095068ce51b56233b1a27c50c16d7a7d57f44
LanseylowT <50694634+LanseylowT@users.noreply.github.com>
2023-01-14 18:08:13 +0800

    UserDatabase already cleaned up and hopefully optimized

--- src/Database/Data/GetData.java
+++ src/Database/Data/GetData.java
@@ -1,16 +1,99 @@
 package Database.Data;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class GetData {
-    public List<List<String>> GetUserData() {
+    public List<List<String>> GetUserData() throws IOException {
+        // Variables
+        List<List<String>> records = new ArrayList<>();
+        String[] values;
 
+        // Getting the File Specified
+        FileReader fileReader = new FileReader("C:\\Users\\LGU MACO\\IdeaProjects\\JavaTestSystem\\src\\Users\\Credentials\\Credentials.csv");
 
-        return null;
+        try (CSVReader csvReader = new CSVReader(fileReader)) {
+            while ((values = csvReader.readNext()) != null) {
+                records.add(Arrays.asList(values));
+            }
+            return records;
+        }
     }
 
-    public List<List<String>> GetProductData() {
+    public void AddUserData(String[] credentialData) throws IOException {
+        FileWriter fileWriter = new FileWriter("C:\\Users\\LGU MACO\\IdeaProjects\\JavaTestSystem\\src\\Users\\Credentials\\Credentials.csv", true);
 
-        return null;
+        try (CSVWriter csvWriter = new CSVWriter(fileWriter)) {
+            csvWriter.writeNext(credentialData);
+        }
+        System.out.println("User Date Successfully Added!");
+    }
+
+    public String[][] getCredentials(List<List<String>> records) {
+        String[] username = new String[records.size()];
+        String[] password = new String[records.size()];
+        String[] userID = new String[records.size()];
+
+        for (int i = 0; i < records.size(); i++) {
+            username[i] = records.get(i).get(0);
+            password[i] = records.get(i).get(1);
+            userID[i] = records.get(i).get(2);
+        }
+
+        return new String[][]{username, password, userID};
+    }
+
+    public int VerificationStatus(String name, String password, String[][] credentials, List<List<String>> records) {
+        CredentialVerifier credentialVerifier = new CredentialVerifier();
+        return credentialVerifier.checkCredentials(name, password, credentials, records);
+    }
+
+    public static class CredentialVerifier {
+        public static boolean checkUserNameAndPassword(List<List<String>> records, String username, String password) {
+            for (int i = 1; i < records.size(); i++) {
+                if (Objects.equals(records.get(i).get(0), username) && Objects.equals(records.get(i).get(1), password)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static boolean checkIfUserNameIsUsed(String[] userName, String name) {
+            for (String s : userName) {
+                if (Objects.equals(s, name)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static boolean checkIsPasswordIsUsed(String[] passwordArray, String password) {
+            for (String s : passwordArray) {
+                if (Objects.equals(s, password)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int checkCredentials(String name, String password, String[][] credentials, List<List<String>> records) {
+            if (checkUserNameAndPassword(records, name, password)) {
+                return 1;
+            } else if (checkIfUserNameIsUsed(credentials[0], name)) {
+                return 2;
+            } else if (checkIsPasswordIsUsed(credentials[1], password)) {
+                return 3;
+            }
+            return 0;
+        }
     }
 }
+
--- src/Users/UserDatabase.java
+++ src/Users/UserDatabase.java
@@ -1,13 +1,12 @@
 package Users;
 
-import com.opencsv.CSVReader;
-import com.opencsv.CSVWriter;
+import Database.Data.GetData;
 import org.apache.commons.lang3.RandomStringUtils;
 
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class UserDatabase {
     // Global Variable
@@ -21,6 +20,9 @@ public class UserDatabase {
         // Variables
         String name, password, randomID;
         List<List<String>> records = new ArrayList<>();
+        GetData data = new GetData();
+        records = data.GetUserData();
+        int verificationStatus;
 
         // Input
         /* Checking Credentials */
@@ -30,84 +32,22 @@ public class UserDatabase {
         System.out.print("Enter Password: ");
         password = input.nextLine();
         randomID = RandomStringUtils.randomNumeric(6);
+        String[] credentialData = {name, password, randomID};
 
+        // Dili ko sure kung unsay gamit ani karon, basta naka 2d array na ang mga credentials
+        String[][] credentials = data.getCredentials(records); // [[Usernames] [Passwords] [User ID's]] in order
+        verificationStatus = data.VerificationStatus(name, password, credentials, records);
 
-        FileWriter fileWriter = new FileWriter("C:\\Users\\LGU MACO\\IdeaProjects\\JavaTestSystem\\src\\Users\\Credentials\\Credentials.csv", true);
-        FileReader fileReader = new FileReader("C:\\Users\\LGU MACO\\IdeaProjects\\JavaTestSystem\\src\\Users\\Credentials\\Credentials.csv");
-
-        try (CSVWriter csvWriter = new CSVWriter(fileWriter)) {
-            try (CSVReader csvReader = new CSVReader(fileReader)) {
-                String[] values;
-                while ((values = csvReader.readNext()) != null) {
-                    records.add(Arrays.asList(values));
-                }
-                // Dili ko sure kung unsay gamit ani karon, basta naka 2d array na ang mga credentials
-                String[][] credentials = getCredentials(records); // [[Usernames] [Passwords] [User ID's]] in order
-
-                switch (checkCredentials(name, password, credentials, records)) {
-                    case 1 -> System.out.println("You are already logged in!");
-                    case 2 -> System.out.println("Username is already taken!");
-                    case 3 -> System.out.println("Password is already taken!");
-                    case 0 -> {
-                        // Adding Credentials
-                        String[] credentialData = {name, password, randomID};
-                        csvWriter.writeNext(credentialData);
-                    }
-                    default -> System.out.println("Invalid Input!");
-                }
+        switch (verificationStatus) {
+            case 1 -> System.out.println("You are already logged in!");
+            case 2 -> System.out.println("Username is already taken!");
+            case 3 -> System.out.println("Password is already taken!");
+            case 0 -> {
+                // Adding Credentials
+                data.AddUserData(credentialData);
             }
+            default -> System.out.println("Invalid Input!");
         }
     }
 
-    public static int checkCredentials(String name, String password, String[][] credentials, List<List<String>> records) {
-        if (checkUserNameAndPassword(records, name, password)){
-            return 1;
-        } else if (checkIfUserNameIsUsed(credentials[0], name)) {
-            return 2;
-        } else if (checkIsPasswordIsUsed(credentials[1], password)) {
-            return 3;
-        }
-        return 0;
-    }
-
-    public static boolean checkUserNameAndPassword(List<List<String>> records, String username, String password){
-        for (int i = 1; i < records.size(); i++) {
-            if (Objects.equals(records.get(i).get(0), username) && Objects.equals(records.get(i).get(1), password)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean checkIfUserNameIsUsed(String[] userName, String name){
-        for (String s : userName) {
-            if (Objects.equals(s, name)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean checkIsPasswordIsUsed(String[] passwordArray, String password){
-        for (String s : passwordArray){
-            if (Objects.equals(s, password)){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static String[][] getCredentials(List<List<String>> records){
-        String[] username = new String[records.size()];
-        String[] password = new String[records.size()];
-        String[] userID = new String[records.size()];
-
-        for (int i = 0; i < records.size(); i++) {
-            username[i] = records.get(i).get(0);
-            password[i] = records.get(i).get(1);
-            userID[i] = records.get(i).get(2);
-        }
-
-        return new String[][]{username, password, userID};
-    }
 }
